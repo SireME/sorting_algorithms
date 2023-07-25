a9b65be83d7c97d5a71a72e45c8aed003a7a0124
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-25 20:08:56 +0100

    ensure just single list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *linkn, *nlist;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (*list == NULL)
 		return;
 
 	nlist = *list;
