e61d34ba3c29d840212bfcda50514dc5d8d34375
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-25 20:06:04 +0100

    fix: big O file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *linkn, *nlist;
 
-	if (*list == NULL || (*list)->next == NULL || (*list)->next->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 
 	nlist = *list;
