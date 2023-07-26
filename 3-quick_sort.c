d5a375aa06f28af9a3cd7192f1fb7611604af062
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-26 03:59:27 +0100

    quicksort implementation

---
+++ 3-quick_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * quick_sort - implementation of quicksort algorithm
+ * @array: array to sort
+ * @size: size of array to sort
+ *
+ * Return: nothing
+ */
+
+int breakdown(int arr[], int low, int high) {
+	int temp, pivot = arr[high];  /* pivot for algo*/
+	int i = low - 1;       /*lower bound idx*/
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		/*Instance when current element is <= pivot*/
+		if (arr[j] <= pivot)
+		{
+			i++;
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	/*swap arr[i + 1] and arr[high] (or pivot)*/
+	temp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = temp;
+	print_array(array, size);
+	return (i + 1);
+}
+
+void quick_sort_i(int* arr, int low, int high)
+{
+	int bd;
+
+	if (low < high)
+	{
+		bd = breakdown(arr, low, high);
+		quick_sort_i(arr, low, bd - 1);
+		quick_sort_i(arr, bd + 1, high);
+	}
+}
+
+void quick_sort(int *array, size_t size)
+{   
+	if (array == NULL)
+		return;
+
+	/* sort wrapper*/
+	quick_sort_i(array, 0, size - 1);
+}
